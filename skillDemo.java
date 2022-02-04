58cf8adf28c9ac2b7691e6fbfc72052a8a38a482
Jordan Peranginangin <jordanangin7@gmail.com>
2022-02-04 14:44:55 -0800

    Failing JUnit Test

--- skillDemo.java
+++ skillDemo.java
@@ -1,3 +1,5 @@
 public class skillDemo {
-
+    public static int multiply(int x, int y) {
+        return x*y;
+    }
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class skillDemoTest {
+    @Test
+    public void multiply() {
+        assertEquals(2, skillDemo.multiply(2,2));
+    }
+}
